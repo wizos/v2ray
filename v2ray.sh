30116c49e6e308ec8f16e8f5dbe6b865998592de
Mcl <mixool0204@gmail.com>
2020-08-28 18:23:07 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -17,63 +17,32 @@ EOF
 # config v2ray
 cat << EOF > /usr/bin/v2ray/config.json
 {
-    "inbounds": [
+    "inbounds": 
+    [
         {
-            "port": 10086,
-            "listen": "127.0.0.1",
-            "protocol": "vmess",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "$UUID"
-                    }
-                ]
-            },
+            "port": 10086,"listen": "127.0.0.1","protocol": "vless",
+            "settings": {"clients": [{"id": "$UUID"}],"decryption": "none"},
             "streamSettings": {"network": "ws","wsSettings": {"path": "$WSPATH"}}
         }
     ],
-
-    "outbounds": [
-        {
-            "protocol": "freedom"
-        },
-        {
-            "protocol": "blackhole",
-            "tag": "blocked"
-        },
-        {
-            "protocol": "socks",
-            "tag": "sockstor",
-            "settings": {
-                "servers": [
-                    {
-                        "address": "127.0.0.1",
-                        "port": 9050
-                    }
-                ]
-            }
-        }
+	
+    "outbounds": 
+    [
+        {"protocol": "freedom","tag": "direct","settings": {}},
+        {"protocol": "socks","tag": "sockstor","settings": {"servers": [{"address": "127.0.0.1","port": 9050}]}},
+        {"protocol": "blackhole","tag": "blocked","settings": {}}
     ],
-
-    "routing": {
-        "rules": [
-            {
-                "type": "field",
-                "outboundTag": "sockstor",
-                "domain": [
-                    "geosite:tor"
-                ]
-            },
-            {
-                "type": "field",
-                "outboundTag": "blocked",
-                "domain": [
-                    "geosite:category-ads-all"
-                ]
-            }
+	
+    "routing": 
+    {
+        "rules": 
+        [
+            {"type": "field","outboundTag": "blocked","ip": ["geoip:private","geoip:cn"]},
+            {"type": "field","outboundTag": "blocked","domain": ["geosite:private","geosite:cn","geosite:category-ads-all"]},
+            {"type": "field","outboundTag": "sockstor","domain": ["geosite:tor"]}
         ]
     }
-}		
+}	
 EOF
 
 # start tor v2ray
